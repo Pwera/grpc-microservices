6dee2b8e9013bef54a20e5c68d414ad90dc847c5
Piotr Wera <piotr.wera@vp.pl>
2019-09-07 23:26:09 +0200

    gRPC java, customer client

---
+++ src/main/java/me/piotr/wera/customer/client/CustomerClient.java
@@ -0,0 +1,89 @@
+package me.piotr.wera.customer.client;
+
+import com.google.common.collect.Streams;
+import com.google.common.util.concurrent.Uninterruptibles;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import me.piotr.wera.customer.CustomerRequest;
+import me.piotr.wera.customer.CustomerResponse;
+import me.piotr.wera.customer.CustomerServiceGrpc;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+public class CustomerClient {
+    private static int messageID = 0;
+
+    public static void main(String[] args) {
+        new CustomerClient().go();
+    }
+
+    private void go() {
+        System.out.println("Starting gRPC Client");
+        final ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+        final CustomerServiceGrpc.CustomerServiceBlockingStub syncClient = CustomerServiceGrpc.newBlockingStub(channel);
+
+        System.out.println("Calling gRPC Unary Method");
+        Stream.of(syncClient.myServiceUnary(constructRequest())).forEach(this::responseFromServer);
+
+
+        System.out.println("Calling gRPC Server Streaming Method");
+        Streams.stream(syncClient.myServiceServerStreaming(constructRequest())).forEach(this::responseFromServer);
+
+        Uninterruptibles.sleepUninterruptibly(1000L, TimeUnit.MILLISECONDS);
+        System.out.println("Calling gRPC Client Streaming Method");
+        final CustomerServiceGrpc.CustomerServiceStub customerServiceStub = CustomerServiceGrpc.newStub(channel);
+        final StreamObserver<CustomerRequest> customerRequestStreamObserver = customerServiceStub.myServiceClientStreaming(new CustomerResponseStreamObserver());
+        customerRequestStreamObserver.onNext(constructRequest());
+        customerRequestStreamObserver.onNext(constructRequest());
+        customerRequestStreamObserver.onNext(constructRequest());
+        customerRequestStreamObserver.onCompleted();
+
+        Uninterruptibles.sleepUninterruptibly(1000L, TimeUnit.MILLISECONDS);
+        System.out.println("Calling gRPC BiDi Streaming Method");
+        final StreamObserver<CustomerRequest> customerRequestBiDiObserver = customerServiceStub.myServiceClientStreaming(new CustomerResponseStreamObserver());
+        customerRequestBiDiObserver.onNext(constructRequest());
+        customerRequestBiDiObserver.onNext(constructRequest());
+        customerRequestBiDiObserver.onNext(constructRequest());
+        customerRequestBiDiObserver.onCompleted();
+
+
+        System.out.println("Shutting down channel");
+        Uninterruptibles.sleepUninterruptibly(4000L, TimeUnit.MILLISECONDS);
+        channel.shutdown();
+
+    }
+
+    private CustomerRequest constructRequest() {
+        return CustomerRequest
+                .newBuilder()
+                .setValue(messageID % 2 == 0 ? "expected" : "not-exxpected")
+                .setId(messageID++)
+                .build();
+    }
+
+    private void responseFromServer(CustomerResponse customerResponse) {
+        System.out.println("Response from server: " + customerResponse.getStatus() + " " + customerResponse.getId());
+
+    }
+
+    private class CustomerResponseStreamObserver implements StreamObserver<CustomerResponse> {
+        @Override
+        public void onNext(CustomerResponse value) {
+            Stream.of("Received message from server ", value).forEach(System.out::println);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+
+        }
+
+        @Override
+        public void onCompleted() {
+
+        }
+    }
+}
---
+++ src/main/java/me/piotr/wera/customer/server/CustomerServer.java
@@ -0,0 +1,126 @@
+package me.piotr.wera.customer.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import me.piotr.wera.customer.CustomerRequest;
+import me.piotr.wera.customer.CustomerResponse;
+import me.piotr.wera.customer.CustomerServiceGrpc;
+
+import java.util.stream.Stream;
+
+public class CustomerServer extends CustomerServiceGrpc.CustomerServiceImplBase {
+
+    private static final String EXPRECTED_VALUE = "expected";
+    private static int messageID = 0;
+    private static int GRPC_SERVER_PORT = 50051;
+
+    public static void main(String[] args) throws Exception {
+        final Server server = ServerBuilder.forPort(GRPC_SERVER_PORT)
+                .addService(new CustomerServer())
+                .build();
+
+        server.start();
+        System.out.println("CustomerServer listening for connection on port " + GRPC_SERVER_PORT);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> CustomerServer.onServerShutdown(server)));
+        server.awaitTermination();
+    }
+
+    @Override
+    public void myServiceUnary(CustomerRequest request, StreamObserver<CustomerResponse> responseObserver) {
+        Stream.of(request, this, "myService function").forEach(System.out::println);
+
+        final CustomerResponse res = CustomerResponse.newBuilder()
+                .setStatus(EXPRECTED_VALUE.equals(request.getValue()))
+                .setId(request.getId() + 1)
+                .build();
+        responseObserver.onNext(res);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void myServiceServerStreaming(CustomerRequest request, StreamObserver<CustomerResponse> responseObserver) {
+        Stream.of(request, this, "myServiceServerStreaming function").forEach(System.out::println);
+
+        Stream.of(true, false, true, true)
+                .map(s -> constructCustomerResponse(s))
+                .forEach(responseObserver::onNext);
+
+
+        responseObserver.onCompleted();
+    }
+
+    private static void onServerShutdown(Server server) {
+        System.out.println("onServerShutdown triggered");
+        server.shutdown();
+        System.out.println("onServerShutdown executed");
+    }
+
+    private CustomerResponse constructCustomerResponse(boolean status) {
+        return CustomerResponse.newBuilder()
+                .setStatus(status)
+                .setId(messageID++)
+                .build();
+    }
+
+    @Override
+    public StreamObserver<CustomerRequest> myServiceClientStreaming(StreamObserver<CustomerResponse> responseObserver) {
+        return new CustomerRequestStreamObserver(responseObserver);
+    }
+
+    private class CustomerRequestStreamObserver implements StreamObserver<CustomerRequest> {
+
+        final StreamObserver<CustomerResponse> responseObserver;
+        String result = "";
+        CustomerRequest latestCustomerRequest;
+
+        public CustomerRequestStreamObserver(StreamObserver<CustomerResponse> responseObserver) {
+            this.responseObserver = responseObserver;
+        }
+
+        @Override
+        public void onNext(CustomerRequest req) {
+            result += "Hello " + req.getValue() + "\n";
+            latestCustomerRequest = req;
+        }
+
+        @Override
+        public void onError(Throwable t) {
+
+        }
+
+        @Override
+        public void onCompleted() {
+            responseObserver.onNext(constructCustomerResponse(false));
+        }
+    }
+
+    @Override
+    public StreamObserver<CustomerRequest> myServiceBiDiStreaming(StreamObserver<CustomerResponse> responseObserver) {
+        return new CustomerRequestStreamObserver2(responseObserver);
+    }
+
+    private class CustomerRequestStreamObserver2 implements StreamObserver<CustomerRequest> {
+
+        final StreamObserver<CustomerResponse> responseObserver;
+
+        public CustomerRequestStreamObserver2(StreamObserver<CustomerResponse> responseObserver) {
+            this.responseObserver = responseObserver;
+        }
+
+        @Override
+        public void onNext(CustomerRequest req) {
+            responseObserver.onNext(constructCustomerResponse(req.getId()%2==0));
+        }
+
+        @Override
+        public void onError(Throwable t) {
+
+        }
+
+        @Override
+        public void onCompleted() {
+            responseObserver.onCompleted();
+        }
+    }
+}
